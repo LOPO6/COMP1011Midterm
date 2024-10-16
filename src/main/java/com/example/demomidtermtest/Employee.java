f1dd94fd8a9a16155d7c561495f1dbc5edce6cf4
Logan <lbpotopn@lakeheadu.ca>
2024-10-16 19:18:25 -0400

    3.1 completed

--- src/main/java/com/example/demomidtermtest/Employee.java
+++ src/main/java/com/example/demomidtermtest/Employee.java
@@ -1,5 +1,6 @@
 package com.example.demomidtermtest;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 public class Employee {
@@ -16,7 +17,13 @@ public class Employee {
     }
 
     public void setEmployeeId(int employeeId) {
-        this.employeeId = employeeId;
+        this.employeeId = employeeId; //this is done automatically here, and an error message will be thrown otherwise another potential way to do this would be like this
+            /*
+             try {
+            this.employeeId = Integer.parseInt(employeeId); // Attempt to parse as an int
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("The employeeID must be an integer.");
+        }*/
     }
 
     public String getFirstName() {
@@ -24,7 +31,13 @@ public class Employee {
     }
 
     public void setFirstName(String firstName) {
-        this.firstName = firstName;
+        if(firstName.length() > 1) {
+
+            this.firstName = firstName;
+        }
+        else{
+            throw new IllegalArgumentException("length of first name must be greater than one");
+        }
     }
 
     public String getLastName() {
@@ -32,32 +45,61 @@ public class Employee {
     }
 
     public void setLastName(String lastName) {
-        this.lastName = lastName;
+        if(lastName.length() > 1) {
+            this.lastName = lastName;
+        }
+        else{
+            throw new IllegalArgumentException("Length Must be longer than 1 character");
+        }
     }
 
     public String getPhoneNumber() {
         return phoneNumber;
+
     }
 
     public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+
+        if(phoneNumber.contains("^(1\\-)?[0-9]{3}\\-?[0-9]{3}\\-?[0-9]{4}$") ) {
+            this.phoneNumber = phoneNumber;
+        }
+        else {
+
+            throw new IllegalArgumentException("This must be in the north american dialing format");
+        }
+
     }
 
     public Date getHireDate() {
         return hireDate;
     }
-
+    private final Date today = new Date();
     public void setHireDate(Date hireDate) {
-        this.hireDate = hireDate;
+        
+        if(hireDate.before(today)) {
+            this.hireDate = hireDate;
+        }
+        else{
+            throw new IllegalArgumentException("Date must be before present");
+        }
     }
+
 
     public String getJobCode() {
         return jobCode;
     }
 
     public void setJobCode(String jobCode) {
-        this.jobCode = jobCode;
+
+
+        if (jobCode.toUpperCase().equals(jobCode) && jobCode.contains("_") && jobCode.indexOf('_') == jobCode.lastIndexOf('_')) {
+           this.jobCode = jobCode;
+        }
+        else {
+            throw new IllegalArgumentException("Must be all capitals and contain one underscore");
+        }
     }
+
 
     public int getSalary() {
         return salary;
