b4ef9aeaf6095164fb96dfb7f34e5ab0a283f75c
Logan <lbpotopn@lakeheadu.ca>
2024-10-16 18:25:53 -0400

    initial commit

---
+++ src/main/java/com/example/demomidtermtest/Employee.java
@@ -0,0 +1,69 @@
+package com.example.demomidtermtest;
+
+import java.util.Date;
+
+public class Employee {
+    private int employeeId;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private Date hireDate;
+    private String jobCode;
+    private int salary;
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Date getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(Date hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public String getJobCode() {
+        return jobCode;
+    }
+
+    public void setJobCode(String jobCode) {
+        this.jobCode = jobCode;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
